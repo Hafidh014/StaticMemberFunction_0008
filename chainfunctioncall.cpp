ca0f2dde71e675c8dbb7d54dd12f3fc4ad2c164c
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-05 16:04:04 +0700

    membuat class judul dan method

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -6,4 +6,11 @@ class buku{
 
 public:
 
+    buku &setJudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul(){
+        return this->judul;
+    }
 };
