d367fc081b31cd489aa1509fee0897144ad8b6bf
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-05 17:59:30 +0700

    memanggil constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -12,3 +12,8 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
 }
+
+int main(){
+    mahasiswa mhs(113, "Rizqi Tukang Makan");
+    return 0;
+}
