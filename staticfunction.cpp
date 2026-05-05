627f9af20cb3646f64e0b9788ef4b1ccfa887f67
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-05 18:09:01 +0700

    meambuat static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -10,4 +10,21 @@ private:
 public:
     int id;
     string nama;
+
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    }
+
 };
